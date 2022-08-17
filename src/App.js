874243ed329a65d75f7997c1551772641e43303c
nnandinim9 <nnandinim9@gmail.com>
2022-08-18 02:45:18 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,211 @@
+import image from './user.jpg'
+
+import './App.css';
+
+function App() {
+  return (
+   <div>
+  <div class="h1">
+    <span> Employee Details</span>
+  </div>
+
+          
+  <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Leanne Graham<br/>
+            UserName      :   Bret<br/>
+            Employee ID   :   1<br/>
+            phone              :  1-770-736-8031 x56442<br/>
+            email               :  Sincere@april.biz<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Kulas Light<br/>
+              suite          :     Apt. 556<br/>
+              city            :     Gwenborough<br/>
+              zipcode     :     92998-3874<br/>
+                latitude      :     -37.3159<br/>
+                longitude   :      81.1496<br/>
+                </pre>
+          </button>
+       
+
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Ervin Howell<br/>
+            UserName      :   Antonette<br/>
+            Employee ID   :   2<br/>
+            phone              :  010-692-6593 x09125<br/>
+            email               :  Shanna@melissa.tv<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Victor Plains<br/>
+              suite          :     Suite 879<br/>
+              city            :     Wisokyburgh<br/>
+              zipcode     :     90566-7771<br/>
+                latitude      :     -43.9509<br/>
+                longitude   :      -34.4618<br/>
+                </pre>
+          </button>
+         
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Clementine Bauch<br/>
+            UserName      :   Samantha<br/>
+            Employee ID   :   3<br/>
+            phone              :  1-463-123-4447<br/>
+            email               :  Nathan@yesenia.net<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Douglas Extension<br/>
+              suite          :     Suite 847<br/>
+              city            :     McKenziehaven<br/>
+              zipcode     :     59590-4157<br/>
+                latitude      :     -68.6102<br/>
+                longitude   :      -47.0653<br/>
+                </pre>
+          </button>
+         
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Patricia Lebsack<br/>
+            UserName      :   Karianne<br/>
+            Employee ID   :   4<br/>
+            phone              :  493-170-9623 x156<br/>
+            email               :  Julianne.OConner@kory.org<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Hoeger Mall<br/>
+              suite          :     Apt. 692<br/>
+              city            :     South Elvis<br/>
+              zipcode     :     53919-4257<br/>
+                latitude      :     29.4572<br/>
+                longitude   :     -164.2990<br/>
+                </pre>
+          </button>
+   
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Chelsey Dietrich<br/>
+            UserName      :   Kamren<br/>
+            Employee ID   :   5<br/>
+            phone              :  (254)954-1289<br/>
+            email               :  Lucio_Hettinger@annie.ca<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Skiles Walks<br/>
+              suite          :     Suite 351<br/>
+              city            :     Roscoeview<br/>
+              zipcode     :     33263<br/>
+                latitude      :     -31.8129<br/>
+                longitude   :     62.5342<br/>
+                </pre>
+          </button>
+
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Mrs. Dennis Schulist<br/>
+            UserName      :   Leopoldo_Corkery<br/>
+            Employee ID   :   6<br/>
+            phone              :  1-477-935-8478 x6430<br/>
+            email               :  Karley_Dach@jasper.info<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Norberto Crossing<br/>
+              suite          :     Apt. 950<br/>
+              city            :     South Christy<br/>
+              zipcode     :     23505-1337<br/>
+                latitude      :     -71.4197<br/>
+                longitude   :     671.7478<br/>
+                </pre>
+          </button>
+         
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Kurtis Weissnat<br/>
+            UserName      :   Elwyn.Skiles<br/>
+            Employee ID   :   7<br/>
+            phone              :  210.067.6132<br/>
+            email               :  Telly.Hoeger@billy.biz<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Rex Trail<br/>
+              suite          :     Apt. 280<br/>
+              city            :     Howemouth<br/>
+              zipcode     :     58804-1099<br/>
+                latitude      :     24.8918<br/>
+                longitude   :     21.8984<br/>
+                </pre>
+          </button>
+     
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Nicholas Runolfsdottir V<br/>
+            UserName      :   Maxime_Nienow<br/>
+            Employee ID   :   8<br/>
+            phone              :  586.493.6943 x140<br/>
+            email               :  Sherwood@rosamond.me<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Ellsworth Summit<br/>
+              suite          :     Suite 729<br/>
+              city            :     Aliyaview<br/>
+              zipcode     :     451699<br/>
+                latitude      :     -14.3990<br/>
+                longitude   :     -120.7677<br/>
+                </pre>
+          </button>
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Glenna Reichert<br/>
+            UserName      :   Delphine<br/>
+            Employee ID   :   9<br/>
+            phone              :  (775)976-6794 x41206<br/>
+            email               :  Chaim_McDermott@dana.io<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Dayna Park<br/>
+              suite          :     Suite 449<br/>
+              city            :     Bartholomebury<br/>
+              zipcode     :     76495-3109<br/>
+                latitude      :     24.6463<br/>
+                longitude   :     -168.8889<br/>
+                </pre>
+          </button>
+
+          <button className="App">
+          <img src={image} alt="no" height="50" width="50" align="center"/><br/>
+            <pre class="App-logo">
+            Name              :   Clementina DuBuque<br/>
+            UserName      :   Moriah.Stanton<br/>
+            Employee ID   :   10<br/>
+            phone              :  024-648-3804<br/>
+            email               :  Rey.Padberg@karina.biz<br/>
+            address           : </pre>
+            <pre class="App-header">
+              street        :      Kattie Turnpike<br/>
+              suite          :     Suite 198<br/>
+              city            :     Lebsackbury<br/>
+              zipcode     :     31428-22619<br/>
+                latitude      :     -38.2386<br/>
+                longitude   :     57.2232<br/>
+                </pre>
+          </button> 
+   
+    
+   
+          
+        
+        
+</div>
+  );
+}
+export default App;
